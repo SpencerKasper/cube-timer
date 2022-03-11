c24a0b7e9eef18b94bf3d4cf1736e033c70d5ebd
spencerkasper <spener.kasper@gmail.com>
2022-03-10 22:21:00 -0600

    * Add tooltips to describe how the stat values are calculated.

--- src/components/SessionStatistics.tsx
+++ src/components/SessionStatistics.tsx
@@ -27,9 +27,21 @@ const SessionStatistics = () => {
             <h2 className='session-stats-title'>Session Statistics</h2>
             <Card className='stats' variant='outlined'>
                 <CardContent>
-                    <StatValue label='Average of Last 5' statistic={averageOf5Stat}/>
-                    <StatValue label='Average of Last 12' statistic={averageOf12Stat}/>
-                    <StatValue label='Average of All' statistic={averageOfAllSolvesStat}/>
+                    <StatValue
+                        label='Average of Last 5'
+                        description='This is the average of 5 with the fastest and slowest time removed.  The remaining 3 times are averaged together.'
+                        statistic={averageOf5Stat}
+                    />
+                    <StatValue
+                        label='Average of Last 12'
+                        description='This is the average of 12 with the fastest and slowest time removed.  The remaining 10 times are averaged together.'
+                        statistic={averageOf12Stat}
+                    />
+                    <StatValue
+                        label='Average of All'
+                        description='This is the average of all of the times in the log.'
+                        statistic={averageOfAllSolvesStat}
+                    />
                 </CardContent>
             </Card>
         </div>
--- src/components/StatValue.tsx
+++ src/components/StatValue.tsx
@@ -1,14 +1,22 @@
 import {Statistic} from "../utils/stats/Statistic";
 import {TimeFormatter} from "../utils/TimeFormatter";
 import React from "react";
+import {Tooltip} from "@mui/material";
 
-export function StatValue(props: { statistic: Statistic; label: string }) {
+export function StatValue(props: { statistic: Statistic; label: string; description?: string; placementOfDescription?: "bottom-end" | "bottom-start" | "bottom" | "left-end" | "left-start" | "left" | "right-end" | "right-start" | "right" | "top-end" | "top-start" | "top" }) {
     const timeFormatter = new TimeFormatter();
     const statValue = props.statistic.getStatValue();
     return <div className='stat'>
-        <p className='stat-label'>
-            {props.label}:
-        </p>
+        {props.description ?
+            <Tooltip title={props.description} placement={props.placementOfDescription ? props.placementOfDescription : 'left'}>
+                <p className='stat-label'>
+                    {props.label}:
+                </p>
+            </Tooltip> :
+            <p className='stat-label'>
+                {props.label}:
+            </p>
+        }
         <p>
             {statValue ? timeFormatter.getFullTime(statValue) : "-"}
         </p>
