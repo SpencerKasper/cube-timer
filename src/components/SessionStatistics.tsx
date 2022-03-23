fd0f601c7ab84a5503df5b5c8734cee8f752ebfc
spencerkasper <spener.kasper@gmail.com>
2022-03-23 15:07:18 -0500

    * Refactor average time stat

--- src/components/SessionStatistics.tsx
+++ src/components/SessionStatistics.tsx
@@ -3,7 +3,7 @@ import './SessionStatistics.css';
 import {useSelector} from "react-redux";
 import solveSelectors from "../redux/selectors/solveSelectors";
 import {Solve} from "../redux/reducers/solveReducer";
-import {Card, CardActionArea, CardContent, Chip} from "@mui/material";
+import {Card, CardContent} from "@mui/material";
 import {AverageTimeStatistic} from "../utils/stats/AverageTimeStatistic";
 import {StatValue} from "./StatValue";
 import {FastestTimeStatistic} from "../utils/stats/FastestTimeStatistic";
@@ -11,21 +11,7 @@ import {FastestTimeStatistic} from "../utils/stats/FastestTimeStatistic";
 const SessionStatistics = () => {
     const solves: Solve[] = useSelector(solveSelectors.solves);
     const solveTimes = solves.map(solve => solve.time);
-    const withoutMinOrMax = (listOfNumbers: number[]) => {
-        return listOfNumbers
-            .sort((a, b) => a > b ? -1 : 1)
-            .filter((item, index) => index !== 0 && index !== listOfNumbers.length - 1);
-    }
 
-    const mostRecentFiveSolves = solveTimes.length > 4 ? withoutMinOrMax(solveTimes.slice(0, 5)) : solveTimes;
-    const mostRecentTwelveSolves = solveTimes.length > 11 ? withoutMinOrMax(solveTimes.slice(0, 12)) : solveTimes;
-    const mostRecentFiftySolves = solveTimes.length > 49 ? withoutMinOrMax(solveTimes.slice(0, 50)) : solveTimes;
-    const mostRecentHundredSolves = solveTimes.length > 99 ? withoutMinOrMax(solveTimes.slice(0, 100)) : solveTimes;
-    const averageOf5Stat = new AverageTimeStatistic(mostRecentFiveSolves);
-    const averageOf12Stat = new AverageTimeStatistic(mostRecentTwelveSolves, 12);
-    const averageOf50Stat = new AverageTimeStatistic(mostRecentFiftySolves, 50);
-    const averageOf100Stat = new AverageTimeStatistic(mostRecentHundredSolves, 100);
-    const averageOfAllSolvesStat = new AverageTimeStatistic(solveTimes);
     const fastestTimeStat = new FastestTimeStatistic(solveTimes);
     const onStatClick = () => {
         const fastestTimeAsMilliseconds = fastestTimeStat.getStatValueBeforeFormat();
@@ -39,14 +25,14 @@ const SessionStatistics = () => {
             <h2 className='session-stats-title'>Session Statistics</h2>
             <Card className='stats' variant='outlined'>
                 <CardContent className={'stats-container'}>
-                    <StatValue statistic={averageOf5Stat}/>
-                    <StatValue statistic={averageOf12Stat}/>
-                    <StatValue statistic={averageOf50Stat}/>
-                    <StatValue statistic={averageOf100Stat}/>
+                    <StatValue statistic={new AverageTimeStatistic(solveTimes, 5)}/>
+                    <StatValue statistic={new AverageTimeStatistic(solveTimes, 12)}/>
+                    <StatValue statistic={new AverageTimeStatistic(solveTimes, 50)}/>
+                    <StatValue statistic={new AverageTimeStatistic(solveTimes, 100)}/>
                     <StatValue
                         overrideLabelInStat='Average of All'
                         overrideDescriptionInStat='This is the average of all of the times in the log.'
-                        statistic={averageOfAllSolvesStat}
+                        statistic={new AverageTimeStatistic(solveTimes)}
                     />
                     <StatValue onStatClick={onStatClick}
                                onStatClickDescription={'Click to scroll to this solve in the solve log.'}
--- src/utils/stats/AverageTimeStatistic.ts
+++ src/utils/stats/AverageTimeStatistic.ts
@@ -1,16 +1,20 @@
 import {Statistic} from "./Statistic";
+
 const ACCOUNT_FOR_MIN_AND_MAX_REMOVAL = 2;
 
 export class AverageTimeStatistic extends Statistic {
     private numberOfSolvesToAverage: number;
 
-    constructor(times: number[], numberOfSolvesToAverage = 5) {
+    constructor(times: number[], numberOfSolvesToAverage = times.length) {
         super(times);
         this.numberOfSolvesToAverage = numberOfSolvesToAverage;
+        this.times = times.length > numberOfSolvesToAverage - 1 ?
+            this.withoutMinOrMax(times.slice(0, numberOfSolvesToAverage)) :
+            times;
     }
 
     getStatValue(fractionDigits = 2) {
-        if(this.times && this.times.length >= this.numberOfSolvesToAverage - 2) {
+        if (this.times && this.times.length >= this.numberOfSolvesToAverage - 2) {
             const statValue = super.getStatValue();
             if (statValue) {
                 return statValue;
@@ -29,6 +33,12 @@ export class AverageTimeStatistic extends Statistic {
 
     getDescription(): string {
         return `This is the average of ${this.numberOfSolvesToAverage} with the fastest and slowest time removed.  The remaining ${this.numberOfSolvesToAverage - 2} times are averaged together.`
+    }
+
+    withoutMinOrMax = (listOfNumbers: number[]) => {
+        return listOfNumbers
+            .sort((a, b) => a > b ? -1 : 1)
+            .filter((item, index) => index !== 0 && index !== listOfNumbers.length - 1);
     }
 
     private getSumOfSolveTimes() {
